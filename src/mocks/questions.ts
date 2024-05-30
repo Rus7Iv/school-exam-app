5f034af211b6b9615c5dfc905d5a85d5e99f5756
Ruslan Ivanov <rusivary@gmail.com>
2024-05-30 19:53:27 +0400

    replaced questions mocks

---
+++ src/mocks/questions.ts
@@ -0,0 +1,58 @@
+export const questions = [
+  {
+    type: 'single',
+    question: 'Сколько будет 2+2?',
+    options: ['3', '4', '5'],
+    correct: 1,
+  },
+  {
+    type: 'single',
+    question: 'Какого цвета небо?',
+    options: ['Зелёное', 'Синее', 'Красное'],
+    correct: 1,
+  },
+  {
+    type: 'multiple',
+    question: 'Какие из следующих животных являются млекопитающими?',
+    options: ['Кит', 'Акула', 'Слон', 'Крокодил'],
+    correct: [0, 2],
+  },
+  {
+    type: 'short',
+    question: 'Как называется столица России?',
+    correct: 'Москва',
+  },
+  {
+    type: 'long',
+    question:
+      'Опишите основные принципы объектно-ориентированного программирования.',
+  },
+  {
+    type: 'single',
+    question: 'Какой язык программирования используется для веб-разработки?',
+    options: ['Python', 'HTML', 'Java'],
+    correct: 1,
+  },
+  {
+    type: 'multiple',
+    question: 'Выберите известные алгоритмы сортировки.',
+    options: ['Quick Sort', 'Binary Search', 'Merge Sort', 'Dijkstra'],
+    correct: [0, 2],
+  },
+  {
+    type: 'short',
+    question: 'Назовите самую высокую гору в мире.',
+    correct: 'Эверест',
+  },
+  {
+    type: 'long',
+    question:
+      'Опишите, что такое искусственный интеллект и какие задачи он решает.',
+  },
+  {
+    type: 'single',
+    question: 'Какая планета является третьей от Солнца?',
+    options: ['Венера', 'Земля', 'Марс'],
+    correct: 1,
+  },
+]
